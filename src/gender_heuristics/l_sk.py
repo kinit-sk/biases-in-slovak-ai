ffcad01665405c768477fec27217bfc8adcb8bd4
Matúš Pikuliak <matus.pikuliak@gmail.com>
2023-10-23 13:55:16 +0200

    Heuristic improvement

--- src/gender_heuristics/l_sk.py
+++ src/gender_heuristics/l_sk.py
@@ -3,6 +3,7 @@ from gender_heuristics.heuristics import *
 
 heuristic_som = lambda translation, tokens: heuristic_gendered_head(translation, tokens, 'som')
 heuristic_byt = lambda translation, tokens: heuristic_gendered_head(translation, tokens, 'byť')
+heuristic_budem = lambda translation, tokens: heuristic_gendered_head(translation, tokens, 'budem')
 
 heuristic_rad_rada = lambda translation, tokens: heuristic_gendered_pair(translation, tokens, ('rád', 'rada'))
 heuristic_nerad_nerada = lambda translation, tokens: heuristic_gendered_pair(translation, tokens,  ('nerád', 'nerada'))
@@ -11,4 +12,4 @@ heuristic_sam_sama = lambda translation, tokens: heuristic_gendered_pair(transla
 heuristic_som_bol_bola = lambda translation, tokens: heuristic_gender_pair_with_target(translation, tokens, 'som', ('bol', 'bola') )
 
 
-sk_heuristics = [heuristic_som, heuristic_byt, heuristic_rad_rada, heuristic_nerad_nerada, heuristic_sam_sama, heuristic_som_bol_bola]
+sk_heuristics = [heuristic_som, heuristic_byt, heuristic_budem, heuristic_rad_rada, heuristic_nerad_nerada, heuristic_sam_sama, heuristic_som_bol_bola]
